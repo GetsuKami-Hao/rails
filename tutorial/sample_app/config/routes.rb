19f1c3aeceb77220281569a9d25737fa3b4b3647
yirolhao <yirolhao@gmail.com>
2016-10-20 23:52:45 +0800

    用户检验

--- tutorial/sample_app/config/routes.rb
+++ tutorial/sample_app/config/routes.rb
@@ -1,16 +1,19 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   #get 'static_pages/home'
   #get '/home',to: 'static_pages#home'
 
   #get 'static_pages/help'
-  get 'help',to: 'static_pages#help' , as: 'helf'
+  get '/help',to: 'static_pages#help' , as: 'helf'
   
   #get 'static_pages/about'
-  get 'about',to: 'static_pages#about'
+  get '/about',to: 'static_pages#about'
 
   #get 'static_pages/contact'
-  get 'contact',to: 'static_pages#contact'
-  
+  get '/contact',to: 'static_pages#contact'
+ 
+ 	get '/signup', to: 'users#new'
   root 'static_pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
